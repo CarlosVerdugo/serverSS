ced3ef1b29a6584eaf887f61d9e0d2315b494160
Carlos <carlos.verdugom@usm.cl>
2023-09-05 20:54:55 -0300

    new

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import "dotenv/config"
 import { ApolloServer } from "@apollo/server"
 import { startStandaloneServer } from "@apollo/server/standalone"
 import mongoose from "mongoose"
+import express from 'express';
 
 import { types, inputs, queries, mutations } from "./typedefs/index.js"
 
@@ -25,6 +26,11 @@ const server = new ApolloServer({
 
 const app = express();
 server.applyMiddleware({ app });
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Conexión exitosa a MongoDB'))
+  .catch(err => console.error('Error de conexión a MongoDB', err));
+
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
